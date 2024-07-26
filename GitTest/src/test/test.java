8496ae000b2748b54809bea8c9d7a681810dcd9a
user1 <user1@DESKTOP-JC9U22B>
2024-07-26 17:02:05 +0900

    코드변경

--- GitTest/src/test/test.java
+++ GitTest/src/test/test.java
@@ -3,5 +3,8 @@ package test;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("TEST");
+		System.out.println("TEST");
+		System.out.println("TEST");
+		System.out.println("TEST");
 	}
 }
